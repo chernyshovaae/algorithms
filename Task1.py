b9e5acbe3be869cdefb7b04708e8e8f5fc31fe55
tt <gosevenday@mail.ru>
2021-08-18 01:05:39 +0300

    tt

--- Task1.py
+++
@@ -1,8 +0,0 @@
-val = int(input('Input three-digit number:\n'))
-
-if (val / 1000) > 0 and (val / 1000) < 1:
-    sum_val = (val // 100) + ((val % 100) // 10) + (val % 10)
-    prod_val = (val // 100) * ((val % 100) // 10) * (val % 10)
-    print(f'Sum: {sum_val}, Product: {prod_val}')
-else:
-    print('Input three-digit number')
--- Task8.py
+++
@@ -1,9 +0,0 @@
-year_ = int(input('Input year:\n'))
-
-if (val / 10000) > 0 and (val / 10000) < 1:
-    if year_ % 4 == 0:
-        print('Temporal year')
-    else:
-        print('Non-temporal year ')
-else:
-    print('Input four -digit number')
